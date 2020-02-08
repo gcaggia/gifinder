0ef5d3db9b98dd7a6bf47cd8ef36ee1843d66131
Guillaume Caggia <guillaume.caggia@gmail.com>
2020-02-08 19:47:24 +1100

    PR-2 add code for header

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React    from 'react';
+import {Navbar} from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+      <Navbar bg="dark" variant="dark" className="p-3 justify-content-between">
+        <Navbar.Brand href="#" className="pl-3">Gifinder</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <a href="#">Fork me on Github</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   );
 };
 
