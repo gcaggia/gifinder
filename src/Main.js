d90c04792b61b529da582a6fa6f257a91c6a6ab4
Guillaume Caggia <guillaume.caggia@gmail.com>
2020-02-08 19:51:01 +1100

    PR-2 add code for basic main structure

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React                 from 'react';
+import {Col, Container, Row} from 'react-bootstrap';
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className="mt-3 pt-5">
+      <Container>
+        <Row>
+          <Col md={{ span: 10, offset: 1 }}>
+            Main app
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
