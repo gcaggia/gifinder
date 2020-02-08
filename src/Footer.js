e2514d464d729966b44469484c00f2aaafc32e4c
Guillaume Caggia <guillaume.caggia@gmail.com>
2020-02-08 19:58:51 +1100

    PR-2 add code for basic footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React    from 'react';
+import {Navbar} from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="">
+      <Navbar bg="dark" fixed="bottom" sticky="bottom" variant="dark" className="p-5">
+        <div className="text-white pl-5">
+          <span>Guillaume | </span>
+          <a href="http://codé.com/">codé.com</a>
+        </div>
+      </Navbar>
+    </footer>
   );
 };
 
