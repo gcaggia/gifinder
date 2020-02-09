5da530bf62fdb65e2d2e268d56990a3cb1e859f2
Guillaume Caggia <guillaume.caggia@gmail.com>
2020-02-09 17:12:36 +1100

    PR-4 design logo search

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,32 @@
 import React                    from 'react';
 import {Button, Col, Form, Row} from 'react-bootstrap';
+import { IconContext } from "react-icons";
+import { FaSearch } from 'react-icons/fa'
 
 const SearchForm = ({searchTerm, onChangeSearchTerm, onSubmitSearch}) => {
   return (
     <Row>
-      <Col md={{ span: 9, offset: 2 }}>
+      <Col xs={12} sm={{ span: 9, offset: 2 }}>
+        <Row className="logo-row">
+          <Col xs={{ span: 9, offset: 1 }} className="logo-col text-center">
+            <IconContext.Provider value={{ color: "#d1d8e0", size: "55px" }}>
+              <FaSearch className="logo-icon" />
+            </IconContext.Provider>
+            <h1 className="logo-text">
+              <span className="logo-letter-g" >G</span>
+              <span className="logo-letter-i1">I</span>
+              <span className="logo-letter-f" >F</span>
+              <span className="logo-letter-i2">i</span>
+              <span className="logo-letter-n" >n</span>
+              <span className="logo-letter-d" >d</span>
+              <span className="logo-letter-e" >e</span>
+              <span className="logo-letter-r" >r</span>
+            </h1>
+          </Col>
+        </Row>
         <Form>
           <Form.Row>
-            <Col xs={9} sm={{ span: 6, offset: 2 }} >
+            <Col xs={{ span: 6, offset: 2 }} >
               <Form.Control
                 size="lg"
                 placeholder="Search your favourite GIF"
@@ -22,7 +41,7 @@ const SearchForm = ({searchTerm, onChangeSearchTerm, onSubmitSearch}) => {
                 type="submit"
                 onClick={(e) => onSubmitSearch(e)}
               >
-                Submit
+                Search
               </Button>
             </Col>
           </Form.Row>
