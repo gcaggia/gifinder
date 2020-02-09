6c02b30612801ec628a0aa2e38e3794300bdfec4
Guillaume Caggia <guillaume.caggia@gmail.com>
2020-02-09 20:07:56 +1100

    Updates

---
+++ precache-manifest.c8ae66c99bf49f8eb54cb9f4b868fb1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2451ceed57caba8938e25a26552d41",
+    "url": "/gifinder/index.html"
+  },
+  {
+    "revision": "2ecbd726ee1e274bc0b8",
+    "url": "/gifinder/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a7b65984f8c20fa5a209",
+    "url": "/gifinder/static/css/main.1970ae25.chunk.css"
+  },
+  {
+    "revision": "2ecbd726ee1e274bc0b8",
+    "url": "/gifinder/static/js/2.aa96037b.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/gifinder/static/js/2.aa96037b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b65984f8c20fa5a209",
+    "url": "/gifinder/static/js/main.cf89a9cb.chunk.js"
+  },
+  {
+    "revision": "b12ad77bbfe5f478a422",
+    "url": "/gifinder/static/js/runtime-main.0803bdb6.js"
+  }
+]);
